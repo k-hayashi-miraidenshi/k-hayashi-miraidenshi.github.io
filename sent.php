fb7a8ffa5b8e0d5aac667ea06f5e14ae7760d6d3
k-hayashi-miraidenshi <49140578+k-hayashi-miraidenshi@users.noreply.github.com>
2019-04-28 16:07:00 +0900

    Update sent.php

--- sent.php
+++ sent.php
@@ -16,27 +16,20 @@
       </ul>
     </div>
   </div>
-
   <div class="main">
     <div class="thanks-message">お問い合わせいただきありがとうございます。</div>
     <div class="display-contact">
       <div class="form-title">入力内容</div>
-
       <div class="form-item">■ 名前</div>
       <?php echo $_POST['name']; ?>
-
       <div class="form-item">■ 年齢</div>
       <?php echo $_POST['age']; ?>
-
       <div class="form-item">■ お問い合わせの種類</div>
-      <!-- この下でcategoryを受け取りechoしましょう -->
        <?php echo $_POST['category']; ?>
-      
       <div class="form-item">■ 内容</div>
       <?php echo $_POST['body']; ?>
     </div>
   </div>
-
   <div class="footer">
     <div class="footer-left">
       <ul>
